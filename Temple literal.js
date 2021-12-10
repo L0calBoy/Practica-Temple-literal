c3f0cf267ac5d4919684e160a9571d1c07dbbeb1
LocalBoy <68517729+L0calBoy@users.noreply.github.com>
2021-12-09 23:58:11 -0500

    Add files via upload

---
+++ Temple literal.js
@@ -0,0 +1,22 @@
+"use strict";
+(function () {
+    function getEdad() {
+        return 100 + 100 + 100 + 1;
+    }
+    const nombre = "Jose";
+    const edad = 20;
+    const apellido = "Arias";
+    // Jose Arias (Edad:33)
+    //const salida = nombre +" "+apellido+" "+"(Edad:"+edad+")";
+    // sin temple literal
+    // baktik alt  96 / 139 
+    //const salida = `${nombre} \n${apellido} \n(Edad:${edad})`;
+    // alt 92
+    const salida = `    
+${nombre} 
+${apellido} 
+(Edad:${getEdad()})`;
+    // dentro de las { es codigo puro puedo hacer lo que quiera con esas variables}
+    // tambien hace salto de linea con enter :O 
+    console.log(salida);
+})();
